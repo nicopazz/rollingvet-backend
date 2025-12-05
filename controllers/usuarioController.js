6069fc5861b50d7cedb0047018789419a15b7763
Nicolas Paz <nicopazmalizia@gmail.com>
2025-12-05 10:25:44 -0300

    Funcionalidad crud de usuarios agregada

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,25 +2,25 @@ import Usuario from '../models/Usuario.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-//REGISTRO DE USUARIO
+
 export const crearUsuario = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        //Verifica si el email ya existe
+        
         let usuario = await Usuario.findOne({ email });
         if (usuario) {
             return res.status(400).json({ mensaje: 'Ya existe un usuario con este correo' });
         }
 
-        //Crea el nuevo objeto de usuario
+       
         usuario = new Usuario(req.body);
 
-        //Encripta la contraseña
+        
         const salt = bcrypt.genSaltSync(10); 
         usuario.password = bcrypt.hashSync(password, salt); 
 
-        //Guarda en Base de Datos
+       
         await usuario.save();
 
         res.status(201).json({
@@ -35,36 +35,35 @@ export const crearUsuario = async (req, res) => {
     }
 };
 
-//LOGIN DE USUARIO
+
 export const loginUsuario = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        //Verifica si el email existe
+        
         let usuario = await Usuario.findOne({ email });
         if (!usuario) {
             return res.status(400).json({ mensaje: 'Correo o contraseña incorrectos' });
         }
 
-        //Verifica si la contraseña coincide
+        
         const passwordValido = bcrypt.compareSync(password, usuario.password);
         if (!passwordValido) {
             return res.status(400).json({ mensaje: 'Correo o contraseña incorrectos' });
         }
 
-        //Genera el Token (El pase de seguridad)
-        // Guardamos en el token el ID y el ROL del usuario
+        
         const token = jwt.sign(
             {
                 uid: usuario._id,
                 nombre: usuario.nombre,
                 rol: usuario.role
             },
-            process.env.SECRET_JWT, // La clave secreta del .env
-            { expiresIn: '2h' } // El token expira en 2 horas
+            process.env.SECRET_JWT, 
+            { expiresIn: '2h' } 
         );
 
-        //Responde al frontend con el token
+        
         res.status(200).json({
             mensaje: 'Login exitoso',
             uid: usuario._id,
@@ -76,5 +75,50 @@ export const loginUsuario = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(400).json({ mensaje: 'Error al intentar loguear' });
+    }
+};
+
+
+
+export const listarUsuarios = async (req, res) => {
+    try {
+        
+        const usuarios = await Usuario.find().select('-password');
+        res.status(200).json(usuarios);
+    } catch (error) {
+        res.status(404).json({ mensaje: 'Error al buscar usuarios' });
+    }
+};
+
+export const obtenerUsuario = async (req, res) => {
+    try {
+        const usuario = await Usuario.findById(req.params.id).select('-password');
+        res.status(200).json(usuario);
+    } catch (error) {
+        res.status(404).json({ mensaje: 'No se encontró el usuario' });
+    }
+};
+
+export const editarUsuario = async (req, res) => {
+    try {
+        
+        if(req.body.password){
+             const salt = bcrypt.genSaltSync(10);
+             req.body.password = bcrypt.hashSync(req.body.password, salt);
+        }
+        
+        await Usuario.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json({ mensaje: 'Usuario editado correctamente' });
+    } catch (error) {
+        res.status(400).json({ mensaje: 'Error al editar el usuario' });
+    }
+};
+
+export const borrarUsuario = async (req, res) => {
+    try {
+        await Usuario.findByIdAndDelete(req.params.id);
+        res.status(200).json({ mensaje: 'Usuario eliminado correctamente' });
+    } catch (error) {
+        res.status(404).json({ mensaje: 'Error al eliminar usuario' });
     }
 };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,28 @@
 import { Router } from 'express';
-import { crearUsuario, loginUsuario } from '../controllers/usuarioController.js';
+import { 
+    crearUsuario, 
+    loginUsuario, 
+    listarUsuarios, 
+    borrarUsuario, 
+    editarUsuario, 
+    obtenerUsuario 
+} from '../controllers/usuarioController.js';
+import validarJWT from '../middleware/validarJWT.js';
 
 const router = Router();
 
+// Rutas Públicas
 router.post('/registro', crearUsuario);
 router.post('/login', loginUsuario);
+
+// Rutas Privadas 
+router.route('/usuarios')
+    .get([validarJWT], listarUsuarios) 
+    .post([validarJWT], crearUsuario); 
+
+router.route('/usuarios/:id')
+    .get([validarJWT], obtenerUsuario)
+    .put([validarJWT], editarUsuario)
+    .delete([validarJWT], borrarUsuario);
 
 export default router;
